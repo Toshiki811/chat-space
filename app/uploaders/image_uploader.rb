fb9d124695ba15f9c60348a00dd2d6b940f3117c
Toshiki811 <65066211+Toshiki811@users.noreply.github.com>
2020-07-04 16:28:08 +0900

    S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -15,6 +15,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
 
   process resize_to_fit: [800, 800]
+
+  
 
   
   # Provide a default URL as a default if there hasn't been a file uploaded:
