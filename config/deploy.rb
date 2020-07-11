4d1734547cf809a4e1df6a531e5d4e8f281c600d
Toshiki811 <65066211+Toshiki811@users.noreply.github.com>
2020-07-11 16:59:22 +0900

    Basic認証

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,17 +6,17 @@ lock '3.14.1'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Toshiki811/chat-space.git'
+set :repo_url, 'git@github.com:Toshiki811/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' # カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/Chatspace.pem'] 
+                  keys: ['~/.ssh/Chatspace.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
@@ -32,14 +32,12 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  set :linked_files, %w{ config/secrets.yml }
+  set :linked_files, %w[config/secrets.yml]
 
   desc 'upload secrets.yml'
   task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
+    on roles(:app) do |_host|
+      execute "mkdir -p #{shared_path}/config" if test "[ ! -d #{shared_path}/config ]"
       upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
     end
   end
